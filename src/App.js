eef8429b01060a8338dee9eab181e16e0c9c9c6a
bansal-4572 <ayuban100202@gmail.com>
2024-05-26 21:20:34 +0530

    Fix commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<shops/>}/>
+        <Route path='/mens' element={<shopCategory category="mens"/>}/>
+        <Route path='/womens' element={<shopsCategory category="womens"/>}/>
+        <Route path='/kids' element={<shopsCategoty category="kids"/>}/>
+        <Route path="/product" element={<product/>}>
+        <Route path=":productId" element={<product/>}/>
+     </Route> 
+     <Route path="/cart" element={<cart/>}/>
+     <Route path="/login" element={<loginsignup/>} />
+      </Routes>
+      </BrowserRouter>
+    
+    
     </div>
   );
 }
